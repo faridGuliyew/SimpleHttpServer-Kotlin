86309c4c1d2255d1e0819670211f0dbcdd381f0d
faridGuliyew <faridg@crocusoft.com>
2025-07-13 01:34:18 +0400

    Add MIT LICENSE

--- src/main/java/io/simple/Example.kt
+++ src/main/java/io/simple/Example.kt
@@ -10,7 +10,10 @@ import io.simple.server.*
 import io.simple.utils.findHeader
 
 fun main() {
-    HttpServer(65000) {
+    HttpServer(
+        port = 65000,
+        loggerLevel = LoggerLevel.ALL
+    ) {
 
         install(ContentTypePlugin) {
             setDefaultContentType(ContentTypeValues.JSON)
@@ -41,7 +44,7 @@ fun main() {
 
         delete("/echo") {
              HttpResponse(
-                statusCode = HttpStatusCode.OK,
+                statusCode = HttpStatusCode.INTERNAL_SERVER_ERROR,
                 headers = listOf(HttpHeaders.ContentType(ContentTypeValues.JSON), HttpHeaders.CustomHeaders("Authorization","Bearer 123")),
                 body = request.body
             )
