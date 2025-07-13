d73aebca07bf1685d4b39257696633b686d4d985
faridGuliyew <faridg@crocusoft.com>
2025-07-13 15:31:53 +0400

    Added publish task, increased gradle version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,11 @@
 plugins {
     id("java")
+    id("maven-publish")
     kotlin("jvm")
 }
 
-group = "com.example"
-version = "1.0-SNAPSHOT"
+group = "io.simple"
+version = "1.0"
 
 repositories {
     mavenCentral()
@@ -22,4 +23,61 @@ tasks.test {
 }
 kotlin {
     jvmToolchain(22)
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+
+            groupId = project.group.toString()
+            artifactId = "simple.http.server"
+            version = project.version.toString()
+
+            pom {
+                name.set("SimpleHttpServer-Kotlin")
+                description.set("A simple server library written purely in Kotlin")
+                url.set("https://github.com/faridGuliyew/SimpleHttpServer-Kotlin")
+
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("https://github.com/faridGuliyew")
+                        name.set("Farid Guliyev")
+                        email.set("ferid.quliyev.sec@gmail.com")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:git://github.com/faridGuliyew/SimpleHttpServer-Kotlin.git")
+                    developerConnection.set("scm:git:ssh://github.com:faridGuliyew/SimpleHttpServer-Kotlin.git")
+                    url.set("https://github.com/faridGuliyew/SimpleHttpServer-Kotlin")
+                }
+            }
+        }
+    }
+
+
+    repositories {
+        maven {
+            name = "SimpleHttpServer-Kotlin"
+            url = uri("https://maven.pkg.github.com/faridGuliyew/SimpleHttpServer-Kotlin")
+
+            credentials {
+                username = (findProperty("repoUser") as String? ?: "")
+                password = (findProperty("repoPassword") as String? ?: "")
+            }
+        }
+    }
 }
